2a795605b6564786a9fe85e7b55f04e478a3c014
maciejr <maciej@deligo.pl>
2025-05-04 18:36:11 +0200

    FIxed primary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#8DC640",
-        secondary: "#50555C",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        "primary-hover": "var(--color-primary-hover)",
+        "primary-active": "var(--color-primary-active)",
+        orange: {
+          50: 'var(--color-orange-50)',
+          100: 'var(--color-orange-100)',
+          200: 'var(--color-orange-200)',
+          300: 'var(--color-orange-300)',
+          400: 'var(--color-orange-400)',
+          500: 'var(--color-orange-500)',
+          600: 'var(--color-orange-600)',
+          700: 'var(--color-orange-700)',
+          800: 'var(--color-orange-800)',
+          900: 'var(--color-orange-900)',
+          950: 'var(--color-orange-950)',
+        },
       },
     },
   },
